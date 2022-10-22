71afc0d754463ea8a968c9c6baea492fa1891fe8
nourr <NourMorgan19@gmail.com>
2022-10-22 04:52:52 +0200

    first commit

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
+
+/**
+ * ViewModel for SleepTrackerFragment.
+ */
+class SleepTrackerViewModel(
+    val database: SleepDatabaseDao,
+    application: Application
+) : AndroidViewModel(application) {
+
+    // make the coroutine Job
+    private val viewModelJob = Job()
+
+    // define the coroutine scope "ui_scope"
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    // to hold the current night when the view model is created
+    private var tonight = MutableLiveData<SleepNight?>()
+
+
+    // since database.getAllNights() returns a LiveData<List<SleepNight>>
+    private val nights = database.getAllNights()
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    // init block to initialize a night as soon as the view model is called
+    init {
+        initializeTonight()
+    }
+
+    //  initializeTonight() will run in the ui-Scope coroutine
+    private fun initializeTonight() {
+        uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    /*
+    1- make it suspend fun , runs in the Dispatchers.IO context
+    2- Let the coroutine get tonight from the database.
+    If the start and end times are the not the same,
+    meaning, the night has already been completed,
+    return null. Otherwise, return night
+     */
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        return withContext(Dispatchers.IO) {
+            var night = database.getToNight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    //  onStartTracking(), the click handler for the Start button
+    /*create a new night when start button is clicked the set
+    *the  tonight.value = getTonightFromDatabase()
+    *so that it's updated to the last night
+    */
+    fun onStartTracking() {
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(newNight: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(newNight)
+        }
+    }
+
+
+    /*
+    * If it hasn't been set yet,
+    * set the endTimeMilli to the current system time and call update() with the night.
+    * */
+    fun onStopTracking() {
+        uiScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+            _navigateToSleepQuality.value = oldNight
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+            _showSnackbarEvent.value = true
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+    // to manage navigation
+    // use encapsulation technique
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+
+    // this method acts as a flag to trigger the navigation event
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
+
+    // to show the snackbar onClear
+    private var _showSnackbarEvent = MutableLiveData<Boolean>()
+    val showSnackBarEvent: LiveData<Boolean>
+        get() = _showSnackbarEvent
+
+    fun doneShowingSnackbar() {
+        _showSnackbarEvent.value = false
+    }
+
+    val startButtonVisible = Transformations.map(tonight) {
+        null == it
+    }
+    val stopButtonVisible = Transformations.map(tonight) {
+        null != it
+    }
+    val clearButtonVisible = Transformations.map(nights) {
+        it?.isNotEmpty()
+    }
+
+
+    // to make sure the all coroutines are terminated when the view model is destroyed
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
+
